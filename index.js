178c31c2d5eff94d302f6f24639051322424d2d9
Kritika <kritika@mobisy.com>
2016-10-12 15:36:44 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+var http = require('http')
 var connection = require('./server/server.db_connection.js');
 var routes = require('./server/server.routes.js');
-
-var port = 8000;
 
 var app = express();
 
@@ -18,6 +17,7 @@ app.get('/', function(req,res){
 connection.start();
 routes.configure(app);
 
-app.listen(port || process.env.PORT, function() {
-  console.log("Your sever is running at http://localhost:" + port);
+app.set('port', process.env.PORT || 8000);
+http.createServer(app).listen(app.get('port'), function(){
+	console.log('Express server listening on port ' + app.get('port'));
 });
