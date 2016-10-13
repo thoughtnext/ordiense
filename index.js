f658b7e85048c9e88f72e84fd4091199283f9180
Kritika <kritika@mobisy.com>
2016-10-13 14:27:35 +0530

    Added CORS

--- index.js
+++ index.js
@@ -14,6 +14,19 @@ app.get('/', function(req,res){
 	res.send('hi')
 })
 
+allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  if ('OPTIONS' === req.method) {
+    res.send(200);
+  } else {
+    next();
+  }
+};
+
+app.use(allowCrossDomain);
+
 connection.start();
 routes.configure(app);
 
